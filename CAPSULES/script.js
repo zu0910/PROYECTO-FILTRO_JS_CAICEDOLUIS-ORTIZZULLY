9c1c9d1f1389138b798226171ca4983863630a17
zu0910 <zullyortiz.09@gmail.com>
2024-09-22 21:36:33 -0500

    feat :sparkles: api consumption ready

--- CAPSULES/script.js
+++ CAPSULES/script.js
@@ -40,7 +40,63 @@ function capsule(){
     .then( res => res.json())
     .then(cap => {
         len_data=cap.length;
-    })
-}
+        
 
+        document.querySelector(".reuse").innerHTML=`
+        <p>Reuse Count ${cap[cohete].reuse_count}</p>
+        `
+        document.querySelector(".water").innerHTML=`
+        <p>Water landings ${cap[cohete].water_landings}</p>
+        `
+        document.querySelector(".land").innerHTML=`
+        <p>Land landings ${cap[cohete].land_landings}</p>
+        `
+        
+        document.querySelector(".serial").innerHTML=`
+        <p>${cap[cohete].serial}</p>
+        `
+        document.querySelector(".status").innerHTML=`
+        <p>${cap[cohete].status}</p>
+        `
+        document.querySelector(".type").innerHTML=`
+        <p>${cap[cohete].type}</p>
+        `
+        document.querySelector(".last_up").innerHTML=`
+        <p>${cap[cohete].last_update}</p>
+
+        `
+        const capsuleId = cap[cohete].id;
+        document.querySelector("#logo").innerHTML = "";
+        fetch(`https://api.spacexdata.com/v4/launches`)
+            .then(res => res.json())
+            .then(launches => {
+                const laun = launches.find(launch => launch.capsules.includes(capsuleId));
+
+                document.querySelector("#name").innerHTML=`
+                <p> ${laun.name} </p>
+                `
+                document.querySelector("#logo").innerHTML = `
+                <img src="${laun.links.patch.small}">
+                `;
+
+                document.querySelector("#details").innerHTML=`
+                <p> ${laun.details}</p>
+                `
+
+
+                let carouselHTML = ''; // Almacena todas las imágenes temporalmente
+
+                laun.links.flickr.original.forEach((img) => {
+                    carouselHTML += `
+                        <div class="carousel__item">
+                            <img src="${img}" referrerpolicy="no-referrer">
+                        </div>
+                    `;
+                });
+                
+                document.querySelector("#imagenes").innerHTML = carouselHTML; 
+            });
+    })
+
+}
 capsule()
